ec367e35de142856cb4d619c806f6227f8bade79
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-10-10 23:19:41 +0100

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 function App() {
+  const [isMenuOpen, setMenuOpen] = useState(false)
+  function close() {
+    setMenuOpen(false);
+  }
+
+  function toggleMenu() {
+    setMenuOpen(!isMenuOpen);
+  }
+
   return (
     <div>
-      <h1>Jolt</h1>
+      <header>
+        <nav>
+          {/* <h1 id="logo"><span><img src={logo} alt="logo" /></span> Tokoo </h1> */}
+          <h1>JOLT</h1>
+          <input type="checkbox" id="check" checked={isMenuOpen} onChange={toggleMenu} />
+          <ul className={`menu ${isMenuOpen ? "open" : "closed"}`}>
+            <li> 
+              <a href="#">Categories: <input type="text" placeholder="Search Product" /></a> 
+              <a href="#" id="search" onClick={close}> 
+                <ion-icon name="search-outline"></ion-icon> 
+              </a> 
+            </li>
+            <label htmlFor="check" className="close-menu">
+              <ion-icon name="close-outline"></ion-icon>
+            </label>
+          </ul>
+          <label htmlFor="check" className="open-menu">
+            <ion-icon name="menu-outline"></ion-icon>
+          </label>
+        </nav>
+      </header>
     </div>
   )
 }
