9aa11ebf94b8065ec6820cfe84e27d492cb6aef5
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-10-14 12:11:59 +0100

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing";
 import SiteFooter from "./pages/footer";
 import ProductsScreen from "./pages/product";
+import ScrollToTop from "./pages/scrolltotop";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/product" element={<ProductsScreen />} />
